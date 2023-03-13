90bf87af974a94ed276769990d938483d727c505
bismishameer <73056982+bismishameer@users.noreply.github.com>
2023-03-13 14:31:09 +0530

    this is my first commit

---
+++ myproject/demoapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from store.models import Product
+# Create your views here.
+
+def home(request):
+    product = Product.objects.all().filter(is_available=True)
+
+    context = {
+
+        'products': product,
+    }
+
+    return render(request,'home.html',context)
+
